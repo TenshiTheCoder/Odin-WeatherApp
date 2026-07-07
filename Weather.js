234fe45146650c08d6d37fd3e2ee65d125f57c8e
TenshiTheCoder <tenshibusinessbrand@gmail.com>
2026-07-06 20:05:50 -0400

    feat: add weather location management and temperature unit switching
    - Added dynamic weather form with multiple location inputs
    - Added weather data storage for switching between locations
    - Added location dropdown for viewing saved forecasts
    - Added unit selector for US and metric weather data
    - Updated API requests to support date ranges and unit preferences
    - Improved weather display to handle dynamic temperature units

--- Weather.js
+++ Weather.js
@@ -2,19 +2,26 @@
 const weatherDiv = document.querySelector(".weather-display");
 const weatherSearch = document.querySelector("#weather-search");
 const searchBtn = document.querySelector("#search-btn");
+const formBtn = document.querySelector("#form-btn");
 
-async function retrieveWeather(location, dateOne, dateTwo) {
+const weatherDataStore = {};
+let locationSelector;
+let currentDateOne = "";
+let currentDateTwo = "";
+
+async function retrieveWeather(location, dateOne, dateTwo, unit) {
     try {
-        let baseURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/`;
-        if(dateOne){
+        let baseURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}`;
+        if (dateOne && dateTwo) {
+            baseURL += `/${dateOne}/${dateTwo}`;
+        } else if (dateOne) {
             baseURL += `/${dateOne}`;
         }
 
-        if(dateOne && dateTwo){
-            baseURL += `/${dateTwo}`;
-        }
-
-        baseURL += `?key=S329784KNSJ2P88BJCQAY2UF5`;
+        baseURL += `?unitGroup=${unit}&key=S329784KNSJ2P88BJCQAY2UF5`;
+        console.log("Location:", location);
+        console.log("Date Two:", dateOne);
+        console.log("Date Two:", dateTwo);
         console.log("Final URL is: ", baseURL);
 
         const response = await fetch(baseURL);
@@ -34,8 +41,11 @@ async function retrieveWeather(location, dateOne, dateTwo) {
     }
 }
 
-function displayWeather(data) { 
+function displayWeather(data, unit) { 
     weatherDiv.innerHTML = "";
+
+    const degreeSymbol = unit === "metric" ? "C" : "F";
+
     const addressName = document.createElement("h2");
     addressName.classList.add("address-name");
     addressName.textContent = `Displaying weather for: ${data.address}`;
@@ -54,19 +64,19 @@ function displayWeather(data) {
     dateTime.textContent = "Date / Time";
 
     const maxTemp = document.createElement("th");
-    maxTemp.textContent = "Maximum Temperature (F)";
+    maxTemp.textContent = `Maximum Temperature (${degreeSymbol})`;
 
     const minTemp = document.createElement("th");
-    minTemp.textContent = "Minimum Temperature (F)";
+    minTemp.textContent = `Minimum Temperature (${degreeSymbol})`;
 
     const feelsLike = document.createElement("th");
-    feelsLike.textContent = "Feels Like (F)";
+    feelsLike.textContent = `Feels Like (${degreeSymbol})`;
 
     const feelsLikeMax = document.createElement("th");
-    feelsLikeMax.textContent = "Maximum Feels Like (F)";
+    feelsLikeMax.textContent = `Maximum Feels Like (${degreeSymbol})`;
 
     const feelsLikeMin = document.createElement("th");
-    feelsLikeMin.textContent = "Minimum Feels Like (F)";
+    feelsLikeMin.textContent = `Minimum Feels Like (${degreeSymbol})`;
 
     const dayDesc = document.createElement("th");
     dayDesc.textContent = "Description";
@@ -140,3 +150,193 @@ searchBtn.addEventListener("click", (e) => {
         .then(data => displayWeather(data));
     weatherSearch.value = "";
 })
+
+function showWeatherForm() {
+    let inputCount = 1;
+
+    const weatherDialog = document.createElement("dialog");
+    weatherDialog.classList.add("weather-dialog");
+
+    const weatherForm = document.createElement("form");
+    weatherForm.classList.add("weather-form");
+
+    const locationInputLabel = document.createElement("label");
+    locationInputLabel.htmlFor = "location-input";
+    locationInputLabel.textContent = "Locations: "
+
+    const locationInput = document.createElement("input");
+    locationInput.id = "location-input";
+    locationInput.placeholder = "Search for a 7 day forecast";
+
+    const locationContainer = document.createElement("div");
+    locationContainer.classList.add("location-container");
+    locationContainer.append(locationInputLabel, locationInput);
+
+    const addFieldBtn = document.createElement("button");
+    addFieldBtn.id = "add-field";
+    addFieldBtn.textContent = "+"
+
+    addFieldBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        inputCount++;
+
+        const newInput = document.createElement("input");
+        newInput.id = `location-${inputCount}`;
+        newInput.placeholder = "Enter another location";
+
+        locationContainer.append(newInput);
+    })
+
+    const dateOneInput = document.createElement("input");
+    dateOneInput.id = "date-one";
+    dateOneInput.type ="date";
+
+    const dateTwoInput = document.createElement("input");
+    dateTwoInput.id = "date-two";
+    dateTwoInput.type ="date";
+
+    const degreeSelect = document.createElement("select");
+
+    const fahrenheitOption = document.createElement("option");
+    fahrenheitOption.value = "us";
+    fahrenheitOption.textContent = "°F";
+
+    const celsiusOption = document.createElement("option");
+    celsiusOption.value = "metric";
+    celsiusOption.textContent = "°C";
+
+    degreeSelect.append(fahrenheitOption, celsiusOption);
+
+    const submitBtn = document.createElement("button");
+    submitBtn.type = "submit";
+    submitBtn.classList.add("submit-form");
+    submitBtn.textContent = "Submit";
+
+    const closeBtn = document.createElement("button");
+    closeBtn.classList.add("close-btn");
+    closeBtn.type = "button";
+    closeBtn.textContent = "Close";
+    closeBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        weatherDialog.close();
+    })
+
+    weatherForm.append(
+        locationContainer,
+        addFieldBtn,
+        dateOneInput,
+        dateTwoInput,
+        degreeSelect,
+        submitBtn,
+        closeBtn
+    )
+
+    weatherDialog.appendChild(weatherForm);
+    document.body.appendChild(weatherDialog);
+    weatherDialog.showModal();
+
+    weatherForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const locationInputs = document.querySelectorAll(".location-container input");
+
+    const locations = Array.from(locationInputs)
+        .map(input => input.value)
+        .filter(location => location !== "");
+
+    let firstLocationData = null;
+
+    currentDateOne = dateOneInput.value;
+    currentDateTwo = dateTwoInput.value;
+
+    for (const location of locations) {
+        const data = await retrieveWeather(
+            location,
+            dateOneInput.value,
+            dateTwoInput.value,
+            degreeSelect.value
+        );
+
+        if (data) {
+            weatherDataStore[location] = data;
+
+            if (!firstLocationData) {
+                firstLocationData = data;
+            }
+        }
+    }
+
+    createLocationDropdown();
+    createUnitSelect();
+
+    if (firstLocationData) {
+        displayWeather(firstLocationData);
+    }
+
+    weatherDialog.close();
+});
+
+    weatherDialog.addEventListener("close", () => {
+        weatherDialog.remove();
+    })
+}
+
+formBtn.addEventListener("click", () => {
+    showWeatherForm();
+})
+
+function createLocationDropdown() {
+    locationSelector = document.createElement("select");
+    locationSelector.classList.add("location-selector");
+
+    Object.keys(weatherDataStore).forEach(location => {
+        const option = document.createElement("option");
+        option.value = location;
+        option.textContent = location;
+
+        locationSelector.append(option);
+    });
+
+    locationSelector.addEventListener("change", () => {
+        const selectedLocation = locationSelector.value;
+        displayWeather(weatherDataStore[selectedLocation]);
+    });
+
+    document.body.appendChild(locationSelector);
+}
+
+function createUnitSelect(){
+    const unitSelect = document.createElement("select");
+    
+    const metricOption = document.createElement("option");
+    metricOption.value = "metric";
+    metricOption.textContent = "Metric";
+
+    const usOption = document.createElement("option");
+    usOption.value = "us";
+    usOption.textContent = "US";
+
+    const selectedUnit = unitSelect.value;
+    console.log(selectedUnit);
+
+    unitSelect.append(metricOption, usOption);
+
+    unitSelect.addEventListener("change", async () => {
+        const selectedUnit = unitSelect.value;
+        const selectedLocation = locationSelector.value;
+
+        const data = await retrieveWeather(
+            selectedLocation,
+            currentDateOne,
+            currentDateTwo,
+            selectedUnit
+        );
+
+    if (data) {
+        weatherDataStore[selectedLocation] = data;
+        displayWeather(data, selectedUnit);
+    }
+});
+
+    document.body.appendChild(unitSelect);
+}
